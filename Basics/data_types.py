000321ff0d20778fcde07acd470d018fcfbd04d6
Istiakmondol <theistiakmondol@gmail.com>
2025-03-13 17:31:12 +0600

    tuples from 12th vdo 16:50m

--- Basics/data_types.py
+++ Basics/data_types.py
@@ -8,18 +8,20 @@ boolean
 
 ## Advance Data Types
 '''
-List (it can contain all kind of data types inside it. it behaves like array)
-Tuples
+List/Array ex. lst=["name", 1, 2, 3.4,True] (it can contain all kind of data types inside it. it behaves like array)
+Tuples ex. lst=("name", 1, 2, 3.4,True) (Almost same as List)
 Sets
 Dictionaries
 '''
 
+'''
 ## Booleans
 a=10
 b=12
 print(a==b)
 print(type(a==b))
-
+'''
+'''
 ## List
 lst=["Md.", "Istiak", "Mondol","DOB->",13,2,2001,"Height->",5.7,"Student->",True]
 print(lst[1])
@@ -95,4 +97,40 @@ for i in range(1,11):
     if i%2==0 :
         lst2.append(i)
 print(lst2)
+'''
 
+## Tuples
+numbers=(1,1,2,3,4,5,6,7,8)
+print(type(numbers))
+mixed_tuple=("Md", "Istiak", "mondol", 5, 6, 7, False)
+print(type(mixed_tuple))
+concate_tuple= numbers + mixed_tuple
+print(type(concate_tuple))
+print(concate_tuple)
+
+## Emmutable Nature of Tuples
+## it means one a element assigned, it can't be changed
+## the procedure of changing an element from tuple is (tuple-> list[make changes with element]-> Tuple) 
+## numbers[1]="name" -> this will give you error
+
+## Touple Method
+print(numbers.count(1)) ## it will count the appearance of that item in the touple
+
+print(numbers.index(3)) ## it will show the item at the mentioned index
+
+### packing & unpacking Touple
+packed_tuple=1,"Hello",3.14
+print(packed_tuple)
+
+# unpacking Touple
+a,b,c=packed_tuple
+print(a)
+print(b)
+print(c)
+
+# unpacking Touple using star
+num=(1,2,3,4,5,6)
+first,*middle,last=num
+print(first)
+print(middle)
+print(last)
